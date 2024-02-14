ef8558d196b3d7b5d7b04316e1f15f0b85af16e6
zjones2142 <zachjones2142@gmail.com>
2024-02-14 17:21:29 -0600

    zj - MediaProduct getter/setter generation

--- src/edu/mu/MediaProduct.java
+++ src/edu/mu/MediaProduct.java
@@ -1,10 +1,10 @@
 package edu.mu;
 
 public class MediaProduct {
-	String title;
-    double price;
-    int year;
-    enum genre {
+	public String title;
+    public double price;
+    public int year;
+    public enum genre {
     	ROCK,
     	POP,
     	JAZZ,
@@ -14,11 +14,32 @@ public class MediaProduct {
     	CHILDREN
     }
 
-    public MediaProduct(String title, double price, int year, Genre genre) {
-        this.title = title;
-        this.price = price;
-        this.year = year;
-        this.genre = genre;
+    public MediaProduct() {
+        
     }
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
     
 }
