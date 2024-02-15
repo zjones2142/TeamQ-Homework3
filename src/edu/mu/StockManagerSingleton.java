28e1064b0141032a9daa9c0d37fc9901473a4877
josephmun <josephmun@localhost>
2024-02-14 18:29:52 -0600

    Initial setup for StockManagerSingleton

---
+++ src/edu/mu/StockManagerSingleton.java
@@ -0,0 +1,62 @@
+package edu.mu;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StockManagerSingleton {
+    private static StockManagerSingleton instance;
+    private List<MediaProduct> inventory;
+    private String inventoryFilePath = "inventory.csv"; // Path to your CSV file
+
+    private StockManagerSingleton() {
+        inventory = new ArrayList<>();
+    }
+
+    public static synchronized StockManagerSingleton getInstance() {
+        if (instance == null) {
+            instance = new StockManagerSingleton();
+        }
+        return instance;
+    }
+
+    public boolean initializeStock() {
+        
+    }
+
+    public boolean updateItemPrice(MediaProduct product, double newPrice) {
+        
+    }
+
+    public boolean addItem(MediaProduct product) {
+        return inventory.add(product);
+    }
+
+    public boolean removeItem(MediaProduct product) {
+        return inventory.remove(product);
+    }
+
+    public boolean saveStock() {
+        
+    }
+    
+    public ArrayList<MediaProduct> getMediaProductBelowPrice (int maxPrice) {
+    	
+    }
+    
+    public void printListOfMediaProduct(ArrayList<MediaProduct> productList) {
+    	
+    }
+    
+    public ArrayList<VinylRecordProduct> getVinylRecordList(ArrayList<MediaProduct> productList) {
+    	
+    }
+    
+    public ArrayList<CDRecordProduct> getCDRecordsList(ArrayList<MediaProduct> productList) {
+    	
+    }
+    
+    public ArrayList<TapeRecordProduct> getTapeRecordList(ArrayList<MediaProduct> productList) {
+    	
+    }
+}
--- src/edu/mu/VinylRecordProduct.java
+++ src/edu/mu/VinylRecordProduct.java
@@ -4,9 +4,5 @@ public class VinylRecordProduct extends MediaProduct {
 
     public VinylRecordProduct(String title, double price, int year, Genre genre) {
         super(title, price, year, genre);
-    }
-
-    public VinylRecordProduct(VinylRecordProduct other) {
-        super(other.title, other.price, other.year, other.genre);
-    }
+    }x
 }
